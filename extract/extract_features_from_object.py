3da289c4d20d9c82749e6b9f1e071a32a53092b9
ey321 <eyal.zwecher.44@gmail.com>
2023-03-27 15:54:34 +0300

    basic feature extraction

--- extract/extract_features_from_object.py
+++ extract/extract_features_from_object.py
@@ -1,70 +1,35 @@
 import laspy
 import numpy as np
+from extract import geometry
+import json
+
+FEATURE_X = "feature_x"
+FEATURE_Y = "feature_y"
+FEATURE_Z = "feature_z"
 
 
-def plot_cloud_points_2d(points):
-    pass
+def extract_features_from_object(object_file_path, output_file_path):
+    laspy_file = laspy.read(object_file_path)
+    points = laspy_file.xyz
+    points = geometry.get_center(points)
 
-def center(points):
-    """Return the center of the points"""
-    mean = np.array([points[:, 0].mean(), points[:, 1].mean(), points[:, 2].mean()]).reshape(1, 3)
-    return points - mean
+    points = geometry.remove_outliers(points)
+    x_diff, y_diff, angle = geometry.find_min_bounding_rectangle(points)
+    points = geometry.rotate(points, angle)
 
+    data = {
+        FEATURE_X: x_diff,
+        FEATURE_Y: y_diff,
+        FEATURE_Z: geometry.get_z_diff(points)
+    }
 
-def get_height(points):
-    """Return the height of the points"""
-    return points[:, 2].max() - points[:, 2].min()
-
-
-def rotate(points, angle, center_of_rotation=None):
-    """
-    Rotate the points around the center of rotation around the z axis
-    :param points: the points
-    :param angle: the angle in radians
-    :param center_of_rotation: the point around which to rotate. if set to None, (0,0,0) is used
-    :return: the rotated points
-    """
-    if center_of_rotation is None:
-        center_of_rotation = np.array([0, 0, 0])
-    # translate the points
-    points = points - center_of_rotation
-    # rotate the points
-    rotation_matrix = np.array([[np.cos(angle), -np.sin(angle), 0],
-                                [np.sin(angle), np.cos(angle), 0],
-                                [0, 0, 1]])
-    # apply the rotation matrix to all points
-    points = np.dot(points, rotation_matrix)
-    # translate the points back
-    points = points + center_of_rotation
-    return points
-
-
-def find_min_bounding_box(points):
-    """
-    find the minimal rectangle that contains all points
-    :param points: the points
-    :return: width, height, angle
-    """
-    # get the 2d covariance matrix between x and y
-    cov = np.cov(points[:, 0], points[:, 1])
-    # use the convariance matrix to find the angle
-    angle = np.arctan2(cov[0, 1], cov[0, 0])
-    # rotate the points
-    rotated_points = rotate(points, angle)
-    # find the width and height
-    width = rotated_points[:, 0].max() - rotated_points[:, 0].min()
-    height = rotated_points[:, 1].max() - rotated_points[:, 1].min()
-    return width, height, angle
+    with open(output_file_path, "w") as f:
+        json.dump(data, f, indent=4)
 
 
 def main():
-    filepath = r"..\..\objects\9 - car.las"
-    # load the file using laspy
-    laspy_file = laspy.read(filepath)
-    # get the points
-    points = laspy_file.xyz
-    points = center(points)
-    print(find_min_bounding_box(points))
+    filepath = r"..\..\objects\box\CC-9.las"
+    extract_features_from_object(filepath, "box.json")
 
 
 if __name__ == '__main__':
