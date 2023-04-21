b6130584d714ea1f9dd89b8ec9eddbf394d5e8e5
ey321 <eyal.zwecher.44@gmail.com>
2023-04-21 09:59:47 +0300

    labeling when extracting

--- extract/extract_features_from_object.py
+++ extract/extract_features_from_object.py
@@ -1,3 +1,5 @@
+import os
+
 import laspy
 import numpy as np
 from extract import geometry
@@ -6,9 +8,10 @@ import json
 FEATURE_X = "feature_x"
 FEATURE_Y = "feature_y"
 FEATURE_Z = "feature_z"
+LABEL = "label"
 
 
-def extract_features_from_object(object_file_path, output_file_path):
+def extract_features_from_object(object_file_path, output_file_path, label=None):
     laspy_file = laspy.read(object_file_path)
     points = laspy_file.xyz
     points = geometry.get_center(points)
@@ -20,7 +23,8 @@ def extract_features_from_object(object_file_path, output_file_path):
     data = {
         FEATURE_X: x_diff,
         FEATURE_Y: y_diff,
-        FEATURE_Z: geometry.get_z_diff(points)
+        FEATURE_Z: geometry.get_z_diff(points),
+        LABEL: label
     }
 
     with open(output_file_path, "w") as f:
@@ -28,8 +32,18 @@ def extract_features_from_object(object_file_path, output_file_path):
 
 
 def main():
-    filepath = r"..\..\objects\box\CC-9.las"
-    extract_features_from_object(filepath, "box.json")
+    filepath = r"..\..\objects"
+    i = 0
+    for obj_type in os.listdir(filepath):
+        if os.path.isdir(os.path.join(filepath, obj_type)):
+            obj_type_path = os.path.join(filepath, obj_type)
+            for obj in os.listdir(obj_type_path):
+                if obj.endswith(".las"):
+                    obj_path = os.path.join(obj_type_path, obj)
+                    print(obj_path)
+                    extract_features_from_object(obj_path, "..\\..\\labeled_data\\obj" + str(i).zfill(3) + ".json",
+                                                 obj_type)
+                    i += 1
 
 
 if __name__ == '__main__':
