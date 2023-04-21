33dc5966b0785a620d1937c073eb601c04fdf8de
ey321 <eyal.zwecher.44@gmail.com>
2023-04-21 14:56:14 +0300

    cleanup

--- extract/ransac.py
+++ extract/ransac.py
@@ -4,8 +4,10 @@ import laspy
 from extract import geometry
 from matplotlib import pyplot as plt
 
+THRESHOLD = 0.03
 
-def find_plane(points, n_iteration=200, threshold=0.05):
+
+def find_plane(points, n_iteration=1000, threshold=THRESHOLD):
     """
     Finds the planes in the points
     :param points: the points
@@ -25,47 +27,32 @@ def find_plane(points, n_iteration=200, threshold=0.05):
     return plane
 
 
-def main():
-    laspy_file = laspy.read(
-        r"C:\Users\TLP-241\Desktop\talpiot\magdad\code\extract\subtracted_9_10\filtered.las_COMPONENT_1.las"
-    )
-    points = laspy_file.xyz
-    points = geometry.get_center(points)
-
-    # remove outliers
-    points = geometry.remove_outliers(points)
-
-    # get 10% of the points randomly
-    points = points[np.random.choice(points.shape[0], int(points.shape[0] * 0.1), replace=False), :]
-
-    points = geometry.remove_outliers(points)
-
-    plane = find_plane(points)
-
-    # plot the points and the plane in 3d space
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    # set aspect ratio to 1:1:1
-    ax.set_aspect('equal', 'box')
-    # plot a plane
-    x = np.linspace(-1, 1, 10)
-    y = np.linspace(-1, 1, 10)
-    X, Y = np.meshgrid(x, y)
-    Z = (-plane[0] * X - plane[1] * Y - plane[3]) * 1. / plane[2]
-    ax.plot_surface(X, Y, Z, alpha=0.2)
-    # plot the points
-    ax.scatter(points[:, 0], points[:, 1], points[:, 2], c=points[:, 2], s=0.3)
+def find_plane_inliers(points, plane, threshold=THRESHOLD):
+    """
+    Finds the inliers of the points
+    :param points: the points
+    :param plane: the plane
+    :param threshold: the threshold for the distance
+    :return: the inliers
+    """
+    # find the distance of the points to the plane
+    dist = np.abs(np.dot(points, plane[:3]) + plane[3])
+    # find the inliers
+    inliers = points[dist < threshold, :]
+    return inliers
 
 
-    plt.show()
+def find_plane_outliers(points, plane, threshold=THRESHOLD):
+    """
+    Finds the outliers of the points
+    :param points: the points
+    :param plane: the plane
+    :param threshold: the threshold for the distance
+    :return: the outliers
+    """
+    # find the distance of the points to the plane
+    dist = np.abs(np.dot(points, plane[:3]) + plane[3])
+    # find the outliers
+    outliers = points[dist > threshold, :]
+    return outliers
 
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
-    main()
