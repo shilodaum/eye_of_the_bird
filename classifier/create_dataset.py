2a9a473cdacc02d6770f079bb4d566ec24bb9bbf
T9123847 <omer.dor.043@gmail.com>
2023-05-30 10:09:31 +0300

    changed get_dataset to take relative path and be more specific with the labels

--- classifier/create_dataset.py
+++ classifier/create_dataset.py
@@ -2,38 +2,26 @@ import json
 import os
 import glob
 
-def get_dataset():  # TODO - add docstring
-    """
 
-    :return: The complete dataset in the form of a dictionary.
-     the data is saved under 'data', the labels are saved under 'target'
+def get_dataset():
+    """
+    This function creates a dataset from the json files in the folder 'features'.
+    :return: a dictionary with the data and the labels.
     """
 
     dataset = dict()
     dataset['data'] = list()
     dataset['target'] = list()
-
-    path = r"C:\magdad\eye_of_the_bird\classifier\labeled_data"  # TODO - change to relative path, and add r"" to the path
-    # path = input("Enter path to get  json files from")
-
+    path = os.path.join(os.path.dirname(os.getcwd()), r'data\features')
 
     for file_name in glob.glob(os.path.join(path, '*.json')):
         with open(file_name, encoding='utf-8', mode='r') as curr_file:
             json_file = json.load(curr_file)
             curr_data = list()
-            first = True
-            for feature in json_file:
-                if first:
-                    dataset['target'].append(json_file[feature])
-                    first = False
-                else:
-                    curr_data.append(json_file[feature])
+            dataset['target'].append(json_file['label'])
+            curr_data.append(json_file['feature_x'])
+            curr_data.append(json_file['feature_y'])
+            curr_data.append(json_file['feature_z'])
             dataset['data'].append(curr_data)
 
     return dataset
-
-
-
-
-
-
