669997be6900e87ab272af97671e58b3d5cd99cf
shilodaum <shilodaum@gmail.com>
2023-05-30 15:43:18 +0300

    Densities

--- extract/extract_features_from_object.py
+++ extract/extract_features_from_object.py
@@ -12,34 +12,40 @@ OUT_FOLDER = r"../data/features"
 FEATURE_X = "feature_x"
 FEATURE_Y = "feature_y"
 FEATURE_Z = "feature_z"
-FEATURE_AVG_INTENSITY = "feature_avg_intensity"
 FEATURE_VOLUME = "feature_volume"
 FEATURE_AREA = "feature_area"
 FEATURE_AREA_RATIO = "feature_a_ratio"
 FEATURE_HEIGHT_RATIO = "feature_h_ratio"
+FEATURE_DENSITY = "feature_density"
+FEATURE_AERIAL_DENSITY = "feature_a_density"
 LABEL = "label"
 
 
 def extract_features_from_object(object_file_path, output_file_path, label=None):
     laspy_file = laspy.read(object_file_path)
+
     points = laspy_file.xyz
-    average_intensity = np.mean(laspy_file.intensity)
+
+    intensities = laspy_file.intensity
+    num_points = len(points)
     points = geometry.get_center(points)
 
     points = geometry.remove_outliers(points)
     x_diff, y_diff, angle = geometry.find_min_bounding_rectangle(points)
     z_diff = geometry.get_z_diff(points)
     points = geometry.rotate(points, angle)
-
+    area = x_diff * y_diff
+    volume = x_diff * y_diff * z_diff
     data = {
         FEATURE_X: x_diff,
         FEATURE_Y: y_diff,
         FEATURE_Z: z_diff,
-        FEATURE_AVG_INTENSITY: average_intensity,
-        FEATURE_AREA: x_diff * y_diff,
-        FEATURE_VOLUME: x_diff * y_diff * z_diff,
+        FEATURE_AREA: area,
+        FEATURE_VOLUME: volume,
         FEATURE_AREA_RATIO: y_diff / x_diff,
         FEATURE_HEIGHT_RATIO: z_diff / (x_diff * y_diff),
+        FEATURE_DENSITY: num_points / volume,
+        FEATURE_AERIAL_DENSITY: num_points / area,
         LABEL: label
     }
 
