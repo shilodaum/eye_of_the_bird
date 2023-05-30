8c185ba9581d11f521243f7f99729effbf7e9f1c
shilodaum <shilodaum@gmail.com>
2023-05-30 15:11:09 +0300

    area games

--- extract/extract_features_from_object.py
+++ extract/extract_features_from_object.py
@@ -13,6 +13,10 @@ FEATURE_X = "feature_x"
 FEATURE_Y = "feature_y"
 FEATURE_Z = "feature_z"
 FEATURE_AVG_INTENSITY = "feature_avg_intensity"
+FEATURE_VOLUME = "feature_volume"
+FEATURE_AREA = "feature_area"
+FEATURE_AREA_RATIO = "feature_a_ratio"
+FEATURE_HEIGHT_RATIO = "feature_h_ratio"
 LABEL = "label"
 
 
@@ -24,13 +28,18 @@ def extract_features_from_object(object_file_path, output_file_path, label=None)
 
     points = geometry.remove_outliers(points)
     x_diff, y_diff, angle = geometry.find_min_bounding_rectangle(points)
+    z_diff = geometry.get_z_diff(points)
     points = geometry.rotate(points, angle)
 
     data = {
         FEATURE_X: x_diff,
         FEATURE_Y: y_diff,
-        FEATURE_Z: geometry.get_z_diff(points),
+        FEATURE_Z: z_diff,
         FEATURE_AVG_INTENSITY: average_intensity,
+        FEATURE_AREA: x_diff * y_diff,
+        FEATURE_VOLUME: x_diff * y_diff * z_diff,
+        FEATURE_AREA_RATIO: y_diff / x_diff,
+        FEATURE_HEIGHT_RATIO: z_diff / (x_diff * y_diff),
         LABEL: label
     }
 
