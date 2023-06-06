63cfc85c9a5b1d89a0fbc2e8b31a58357e4847e2
T9123847 <omer.dor.043@gmail.com>
2023-06-06 20:31:35 +0300

    cleaned up the code

--- subtract/CLI_subtract_to_cc.py
+++ subtract/CLI_subtract_to_cc.py
@@ -2,8 +2,11 @@ from subtract import pyCloudCompare as pycc
 from typing import List
 import os
 import laspy
-import numpy as np
 import glob
+from os.path import join as pjoin
+
+SHIFT = [-692300, -3616100, 0]
+BOX = [692297, 3616248, 300, 692335, 3616288, 400]
 
 
 def get_intersection_bounds(cloud1_path: str, cloud2_path: str) -> List[float]:
@@ -46,16 +49,21 @@ def get_intersection_bounds(cloud1_path: str, cloud2_path: str) -> List[float]:
     return [min_x, min_y, min_z, max_x, max_y, max_z]
 
 
-def subtract_clouds(global_shift: List[int], bounds: List[int] = None) -> None:
+def subtract_clouds(input_path: str = r"..\data\scans", output_path: str = r"..\data\objects",
+                    min_points_per_cloud: int = 500, global_shift: List[int] = SHIFT,
+                    bounds: List[int] = None) -> None:
     """
     Subtract 2 clouds, filter the result, and extract connected components from the result.
+    :param input_path: path to the folder containing the 2 clouds to subtract
+    :param output_path: path to the folder to save the result in
+    :param min_points_per_cloud: minimum number of points in a connected component to be saved
     :param global_shift: shift to apply to the clouds before subtracting
     :param bounds: bounds of the area to crop the clouds to. If None, the intersection bounds of the 2 clouds are used.
     :return: None. The result is saved in the output folder.
     """
-    input_path = os.path.join(os.path.dirname(os.getcwd()), r'data\scans')
-    output_path = os.path.join(os.path.dirname(os.getcwd()), r'data\objects')
-    clouds = glob.glob(os.path.join(input_path, '*.las'))
+
+    # Get the paths to the 2 clouds
+    clouds = glob.glob(pjoin(input_path, '*.las'))
     cloud1_path = clouds[0]
     cloud2_path = clouds[1]
 
@@ -78,22 +86,25 @@ def subtract_clouds(global_shift: List[int], bounds: List[int] = None) -> None:
 
     cmd.crop(*shifted_bounds)  # Crop the 2 clouds to the same area
     cmd.c2c_dist()  # Subtract the 2 clouds
-    cmd.save_clouds(output_path + r"\subtracted.las", output_path + r"\reference.las")  # Save the subtracted cloud
+    cmd.save_clouds(pjoin(output_path, r"\subtracted.las"),
+                    pjoin(output_path, r"\reference.las"))  # Save the subtracted cloud
     cmd.clear_clouds()  # Close all opened clouds
 
-    cmd.open(output_path + r"\subtracted.las", global_shift=global_shift)  # Open only the subtracted cloud
+    cmd.open(pjoin(output_path, r"\subtracted.las"), global_shift=global_shift)  # Open only the subtracted cloud
     cmd.filter_sf(0.1, 10)  # Filter the cloud by its calculated distance from its neighbors in the other scan
-    cmd.save_clouds(output_path + r"\filtered.las")  # Save filtered cloud
+    cmd.save_clouds(pjoin(output_path, r"\filtered.las"))  # Save filtered cloud
     cmd.auto_save(True)
-    cmd.extract_cc(8, 100)  # Extract Connected Components from the filtered cloud, and save them
+    cmd.extract_cc(8, min_points_per_cloud)  # Extract Connected Components from the filtered cloud, and save them
     cmd.execute()
 
-    os.remove(output_path + r"\subtracted.las")
-    os.remove(output_path + r"\reference.las")
-    os.remove(output_path + r"\filtered.las")
+    # Delete temporary files
+    os.remove(pjoin(output_path, r"\subtracted.las"))
+    os.remove(pjoin(output_path, r"\reference.las"))
+    os.remove(pjoin(output_path, r"\filtered.las"))
 
 
-def merge_clouds(clouds_path: str, output_path: str, merged_cloud_name: str, global_shift: List[int]) -> None:
+def merge_clouds(clouds_path: str, output_path: str, merged_cloud_name: str,
+                 global_shift: List[int] = SHIFT) -> None:
     """
     Merge multiple clouds into one.
     :param clouds_path: path to the folder containing the clouds to merge
@@ -106,7 +117,7 @@ def merge_clouds(clouds_path: str, output_path: str, merged_cloud_name: str, glo
     cli = pycc.CloudCompareCLI()
     cmd = cli.new_command()
     cmd.silent()  # Disable console
-    cmd.log_file(output_path + r"\log.txt")
+    cmd.log_file(pjoin(output_path, r"\log.txt"))
     cmd.auto_save(False)
     cmd.cloud_export_format("LAS")
 
@@ -114,14 +125,13 @@ def merge_clouds(clouds_path: str, output_path: str, merged_cloud_name: str, glo
     i = 0
     for cloud in os.listdir(clouds_path):
         if cloud.endswith(".las"):
-            cmd.open(clouds_path + "\\" + cloud, global_shift=global_shift)
+            cmd.open(pjoin(clouds_path, cloud), global_shift=global_shift)
         i += 1
         print("Opened " + str(i) + " clouds")
+
     # Merge clouds
     cmd.merge_clouds()
     # Save merged cloud
-    cmd.save_clouds(output_path + "\\" + merged_cloud_name + ".las")
-    print(cmd)
+    cmd.save_clouds(pjoin(clouds_path, merged_cloud_name + ".las"))
+
     cmd.execute()
-
-
